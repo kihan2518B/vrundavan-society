70cec71794f99d78ca9945318a03856a40ffad94
kihan2518B <kp648027@gmail.com>
2026-02-11 21:38:52 +0530

    feat: add clarity tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { Noto_Sans_Gujarati } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -45,6 +46,15 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Vrundavan Park" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vfs6w2y8y2");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${gujarati.className}
